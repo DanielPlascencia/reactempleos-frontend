fcd8120ff3f3550fecd8b3a8e75dbe71a462a72f
JavatoX <cdpm98@hotmail.com>
2023-04-05 12:32:45 -0600

    actualizacion

--- src/helpers/configAxios.js
+++ src/helpers/configAxios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-  baseURL: "https://reactempleos-backend-r0m71aws3-danielplascencia.vercel.app",
+  baseURL:
+    "https://reactempleos-backend-r0m71aws3-danielplascencia.vercel.app/api",
 });
 
 export default clienteAxios;
